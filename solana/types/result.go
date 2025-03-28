fa87909bf62bf62183d724e2530441b959b23ea0
Jeffro <0xjeffro@gmail.com>
2025-03-27 17:49:16 -0700

    refactor

--- solana/types/result.go
+++ solana/types/result.go
@@ -5,32 +5,3 @@ type ParsedResult struct {
 	AccountList []string `json:"accountList"`
 	Actions     []Action `json:"actions"`
 }
-
-type Action interface {
-	GetProgramID() string
-	GetProgramName() string
-	GetInstructionName() string
-}
-
-type BaseAction struct {
-	ProgramID       string `json:"programId"`
-	ProgramName     string `json:"programName"`
-	InstructionName string `json:"instructionName"`
-}
-
-func (a BaseAction) GetProgramID() string {
-	return a.ProgramID
-}
-
-func (a BaseAction) GetProgramName() string {
-	return a.ProgramName
-}
-
-func (a BaseAction) GetInstructionName() string {
-	return a.InstructionName
-}
-
-type UnknownAction struct {
-	BaseAction
-	Error error `json:"error"`
-}
